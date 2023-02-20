0b2cda087934848f14bae6438d1c48b1a27a0709
MrHumble21 <amalikov3@students.wiut.uz>
2023-02-20 13:21:15 +0500

    aaaa

--- src/extras/frontend_constants.js
+++ src/extras/frontend_constants.js
@@ -1,23 +1,23 @@
 const headersCollections = [
-    {label: "Title", key: "title"},
-    {label: "Description", key: "description"},
-    {label: "Tags", key: "tags"},
-    {label: "CommentsID", key: "comments"},
-    {label: "Number of Likes", key: "likes"},
-    {label: "Image Link", key: "image"},
+  { label: "Title", key: "title" },
+  { label: "Description", key: "description" },
+  { label: "Tags", key: "tags" },
+  { label: "CommentsID", key: "comments" },
+  { label: "Number of Likes", key: "likes" },
+  { label: "Image Link", key: "image" },
 ];
 
 const userCollectionHeader = [
-    {label: "Full name", key: "fullName"},
-    {label: "Gender", key: "gender"},
-    {label: "Email", key: "email"},
-    {label: "Phone", key: "phone"},
-    {label: "Password", key: "password"},
-    {label: "Number of Collections", key: "collections"},
-    {label: "Role", key: "role"},
+  { label: "Full name", key: "fullName" },
+  { label: "Gender", key: "gender" },
+  { label: "Email", key: "email" },
+  { label: "Phone", key: "phone" },
+  { label: "Password", key: "password" },
+  { label: "Number of Collections", key: "collections" },
+  { label: "Role", key: "role" },
 ];
 
-const BASE_URl= "https://collections.mrhumble.uz"
+const BASE_URl = "https://rich-tan-sea-urchin-veil.cyclic.app";
 
 exports.BASE_URL = BASE_URl;
 exports.headersCollections = headersCollections;
